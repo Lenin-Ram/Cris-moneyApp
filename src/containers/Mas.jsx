1189c7f9dc7c4e00808def37e826741b8b6f11f0
Lenin-Ramirez <84999784+Lenin-Ram@users.noreply.github.com>
2022-07-20 13:27:13 -0500

    fix background

--- src/containers/Mas.jsx
+++ src/containers/Mas.jsx
@@ -12,9 +12,28 @@ const Mas = () => {
          
          <>
          
-       <h1>hello</h1>
+       <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias soluta magnam non earum error, eos cupiditate nostrum, impedit deleniti quod maiores quis, aperiam beatae in obcaecati adipisci at explicabo possimus.</h1>
+
+
+       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam iure dolores eum aliquam, earum accusamus et iste doloremque fugiat, magnam similique enim quis vero cupiditate porro inventore labore dolorem.</h3>
+       <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam iure dolores eum aliquam, earum accusamus et iste doloremque fugiat, magnam similique enim quis vero cupiditate porro inventore labore dolorem.</h3>
+
+       <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias deleniti excepturi dolorem! Eos ullam autem cumque, necessitatibus at, amet, fugiat beatae quibusdam veniam qui impedit? Odio nostrum exercitationem culpa nulla.</h2>
 
        <Row>
+  <Col sm="6">
+    <Card body>
+      <CardTitle tag="h5">
+        Special Title Treatment
+      </CardTitle>
+      <CardText>
+        With supporting text below as a natural lead-in to additional content.
+      </CardText>
+      <Button>
+        Go somewhere
+      </Button>
+    </Card>
+  </Col>
   <Col sm="6">
     <Card body>
       <CardTitle tag="h5">
